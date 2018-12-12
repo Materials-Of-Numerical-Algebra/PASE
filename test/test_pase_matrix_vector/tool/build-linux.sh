c91d3a64d43d8e8cd0817a7d8bc5b8ce0b9f72b8
Li Yu <liyu@tjufe.edu.cn>
2018-12-12 22:59:42 +0800

    Get dev from hqc-new

--- test/test_pase_matrix_vector/tool/build-linux.sh
+++
@@ -1,4 +0,0 @@
-cmake -Dcmake_module_root=/media/psf/Home/Workspace/PASE/config \
-      -Dpase_root=/media/psf/Home/Workspace/PASE/build-linux/pase \
-      -Duse_hypre=1 -Dhypre_root=/home/ycg/Software/hypre-2.11.2/src/hypre \
-      ../
